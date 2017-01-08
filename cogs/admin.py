859017b5335506128a7fe8d97a2e74033fe5e6d4
TheTrain2000 <gibbstyler7@gmail.com>
2017-01-08 12:52:21 -0600

    Disable loading/unloading cogs for everyone except owner

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,20 +8,12 @@ class Admin():
 
     def __init__(self, bot):
         self.bot = bot
-        # list of all cogs
-        self.all_cogs = [
-            "cogs.games",
-            "cogs.mod",
-            "cogs.misc",
-            "cogs.stats",
-            "cogs.admin",
-            "cogs.reddit",
-            "cogs.logging",
-            "cogs.music"
-        ]
+
+    def check_if_bot_owner(ctx):
+        return ctx.message.author.id == ctx.bot.config["your_user_id"]
 
     @commands.command(hidden=True, pass_context=True)
-    @commands.has_permissions(administrator=True)
+    @commands.check(check_if_bot_owner)
     async def reloadconfig(self, ctx):
         """Reloads the configuration."""
         app_path = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -32,19 +24,19 @@ class Admin():
         await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server, channel=ctx.message.channel)
 
     @commands.command(name='reload', hidden=True, pass_context=True)
-    @commands.has_permissions(administrator=True)
+    @commands.check(check_if_bot_owner)
     async def _reload(self, ctx, *, extension_name : str):
         """Reloads an extension."""
-        if extension_name == "all":
-            for cog in self.all_cogs:
-                try:
-                    self.bot.unload_extension(cog)
-                    self.bot.load_extension(cog)
-                except (AttributeError, ImportError) as e:
-                    await self.bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-            llog = "All cogs reloaded."
-            await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server, channel=ctx.message.channel)
-            return
+        #if extension_name == "all":
+            #for cog in self.all_cogs:
+                #try:
+                    #self.bot.unload_extension(cog)
+                    #self.bot.load_extension(cog)
+                #except (AttributeError, ImportError) as e:
+                    #await self.bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+            #llog = "All cogs reloaded."
+            #await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server, channel=ctx.message.channel)
+            #return
         try:
             self.bot.unload_extension(extension_name)
             self.bot.load_extension(extension_name)
@@ -55,7 +47,7 @@ class Admin():
         await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server, channel=ctx.message.channel)
 
     @commands.command(hidden=True, pass_context=True)
-    @commands.has_permissions(administrator=True)
+    @commands.check(check_if_bot_owner)
     async def load(self, ctx, *, extension_name : str):
         """Loads an extension."""
         try:
@@ -67,7 +59,7 @@ class Admin():
         await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server, channel=ctx.message.channel)
 
     @commands.command(hidden=True, pass_context=True)
-    @commands.has_permissions(administrator=True)
+    @commands.check(check_if_bot_owner)
     async def unload(self, ctx, *, extension_name : str):
         """Unloads an extension."""
         self.bot.unload_extension(extension_name)
@@ -75,7 +67,7 @@ class Admin():
         await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server, channel=ctx.message.channel)
 
     @commands.command(hidden=True)
-    @commands.has_permissions(administrator=True)
+    @commands.check(check_if_bot_owner)
     async def logout(self):
         """Turns off the bot."""
         await self.bot.logout()
