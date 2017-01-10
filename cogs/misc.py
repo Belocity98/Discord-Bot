d8bad0edba7db32797dc755a4d834f991c1033fd
TheTrain2000 <gibbstyler7@gmail.com>
2017-01-09 22:15:48 -0600

    Ability to add strikes to users

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,13 +2,15 @@ from discord.ext import commands
 import discord
 import asyncio
 
+from .utils import checks
+
 class Misc():
 
     def __init__(self, bot):
         self.bot = bot
         self.strikes = {}
 
-    @commands.command(pass_context=True)
+    @commands.group(pass_context=True, no_pm=True, invoke_without_command=True)
     @commands.cooldown(1, 5.1, commands.BucketType.user)
     async def strike(self, ctx, user : discord.Member):
         """Strike somebody. 25 strikes bans for 10 seconds."""
@@ -28,6 +30,28 @@ class Misc():
             self.strikes[user] = 1
             await self.bot.say(str(user) + " now has {} strike.".format(self.strikes[user]))
         llog = "{} striked {}.".format(str(ctx.message.author), str(user))
+        await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server)
+
+    @strike.command(name='add', pass_context=True)
+    @commands.has_permissions(ban_members=True)
+    async def strike_add(self, ctx, user : discord.Member, amount : int):
+        """Add strikes to a user."""
+        server = ctx.message.server
+        if user == server.me:
+            await self.bot.say("Don't try to strike me you cunt.")
+            return
+        if user in self.strikes:
+            self.strikes[user] += amount
+            if self.strikes[user] >= 25:
+                await self.bot.say("```\n{} reached the max strike limit!\n```".format(str(user)))
+                self.strikes[user] = 0
+                await self.bot.get_cog("Mod").ban_func(server, user, message="Reaching 25 strikes.")
+            else:
+                await self.bot.say(str(user) + " now has {} strikes.".format(self.strikes[user]))
+        else:
+            self.strikes[user] = amount
+            await self.bot.say(str(user) + " now has {} strikes.".format(self.strikes[user]))
+        llog = "{} striked {} {} times..".format(str(ctx.message.author), str(user), amount)
         await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server)
 
 def setup(bot):
