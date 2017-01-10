6f5cec1ba455ef90bde474cb419626676cab3ea3
TheTrain2000 <gibbstyler7@gmail.com>
2017-01-09 22:29:54 -0600

    Add ability to remove strikes

--- cogs/misc.py
+++ cogs/misc.py
@@ -51,7 +51,26 @@ class Misc():
         else:
             self.strikes[user] = amount
             await self.bot.say(str(user) + " now has {} strikes.".format(self.strikes[user]))
-        llog = "{} striked {} {} times.".format(str(ctx.message.author), str(user), amount)
+        llog = "{} added {} strikes to {}.".format(str(ctx.message.author), amount, str(user))
+        await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server)
+
+    @strike.command(name='remove', pass_context=True)
+    @commands.has_permissions(ban_members=True)
+    async def strike_remove(self, ctx, user : discord.Member, amount : int):
+        """Remove strikes from a user."""
+        server = ctx.message.server
+        if user == server.me:
+            await self.bot.say("Don't try to strike me you cunt.")
+            return
+        if self.strikes[user] == 0:
+            await self.bot.say("That user has no strikes.")
+            return
+        if user in self.strikes:
+            self.strikes[user] -= amount
+            if self.strikes[user] < 0:
+                self.strikes[user] = 0
+            await self.bot.say(str(user) + " now has {} strikes.".format(self.strikes[user]))
+        llog = "{} removed {} strikes from {}.".format(str(ctx.message.author), amount, str(user))
         await self.bot.get_cog("Logging").do_logging(llog, ctx.message.server)
 
 def setup(bot):
