d6b7900384e71619bde62998ab962a8c67cbae9d
TheTrain2000 <gibbstyler7@gmail.com>
2017-01-19 19:00:49 -0600

    Embeds for message logging

--- cogs/logging.py
+++ cogs/logging.py
@@ -15,15 +15,19 @@ class Logging():
             print("Creating logging channel...")
             await self.bot.create_channel(server, 'bot-logging')
             return
-        if message.author == message.server.me:
-            return
+
         else:
-            lines = []
-            lines.append("**Message Deleted**")
-            lines.append("Author: {}".format(message.author))
-            lines.append("Message: {}".format(message.content))
-            await self.bot.send_message(logging_channel, "\n".join(lines))
-            #await self.bot.send_message(self.bot.get_channel(self.bot.config["channels"]["bot-logging"]), "\n".join(lines))
+            embed = discord.Embed(description=message.content)
+            embed.colour = 0x1BE118 # lucio green
+            embed.set_author(name=message.author, icon_url=message.author.avatar_url)
+            embed.set_footer(text="Message Deleted", icon_url='http://i.imgur.com/ulgDAMM.png')
+            if len(message.attachments) != 0:
+                embed.set_image(url=message.attachments[0]['proxy_url'])
+            if len(message.embeds) != 0:
+                embed = discord.Embed.from_data(message.embeds[0])
+                await self.bot.send_message(logging_channel, content="**Message Deleted**", embed=embed)
+                return
+            await self.bot.send_message(logging_channel, embed=embed)
 
     async def on_message_edit(self, before, after):
         server = before.server
@@ -36,13 +40,13 @@ class Logging():
         if before.content == after.content:
             return
         else:
-            lines = []
-            lines.append("**Message Edited**")
-            lines.append("Author: {}".format(before.author))
-            lines.append("Before: {}".format(before.content))
-            lines.append("After: {}".format(after.content))
-        await self.bot.send_message(logging_channel, "\n".join(lines))
-        #await self.bot.send_message(self.bot.get_channel(self.bot.config["channels"]["bot-logging"]), "\n".join(lines))
+            embed = discord.Embed()
+            embed.add_field(name="Before Content", value=before.content, inline=False)
+            embed.add_field(name='After Content', value=after.content, inline=False)
+            embed.set_footer(text="Message Edited", icon_url='http://i.imgur.com/zWTQEYe.png')
+            embed.colour = 0x1BE118 # lucio green
+            embed.set_author(name=before.author, icon_url=before.author.avatar_url)
+            await self.bot.send_message(logging_channel, embed=embed)
 
     async def do_logging(self, llog, server, channel=None):
         logging_channel = discord.utils.find(lambda c: c.name == 'bot-logging', server.channels)
@@ -50,7 +54,6 @@ class Logging():
             print("Logging channel not found.")
             return
         await self.bot.send_message(logging_channel, llog)
-        #await self.bot.send_message(self.bot.get_channel(self.bot.config["channels"]["bot-logging"]), llog)
         print(llog)
         if channel != None:
             await self.bot.send_message(channel, llog)
