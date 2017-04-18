7e9499f8eb4d8810b1914bf91b1c2517c521e896
TheTrain2000 <gibbstyler7@gmail.com>
2017-04-18 18:29:16 -0500

    Update checks for new database system

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -2,7 +2,12 @@ from discord.ext import commands
 import discord.utils
 
 def is_owner_check(ctx):
-    return ctx.author.id == ctx.bot.config["your_user_id"]
+    try:
+        ctx.bot.db['owner']
+    except KeyError:
+        return True
+
+    return ctx.author.id == ctx.bot.db['owner']
 
 def is_owner():
     return commands.check(lambda ctx: is_owner_check(ctx))
