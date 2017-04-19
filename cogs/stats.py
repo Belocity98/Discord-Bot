d40b8737d7558086bb29d70a55888d374e806ee8
TheTrain2000 <gibbstyler7@gmail.com>
2017-04-18 21:53:49 -0500

    Remove Overwatch statistics

--- cogs/stats.py
+++ cogs/stats.py
@@ -13,36 +13,10 @@ class Stats():
 
     def __init__(self, bot):
         self.bot = bot
-        self.session = aiohttp.ClientSession(loop=bot.loop, headers={'User-Agent': 'TheTrain2000-i<3uFUYU'})
 
     async def on_command(self, ctx):
         self.bot.commands_used[ctx.command.qualified_name] += 1
 
-    @commands.command()
-    async def ostats(self, ctx, battletag : str, mode : str):
-        """Overwatch statistics for a specified user."""
-        url = "https://owapi.net/api/v3/u/" + battletag.replace('#', '-') + "/stats"
-        async with self.session.get(url) as rawdata:
-            data = await rawdata.json()
-        lines = ["```", "{}'s Overwatch Statistics".format(battletag), "----------"]
-        lines.append("Level - {0[overall_stats][level]}")
-        lines.append("Prestige - {0[overall_stats][prestige]}")
-        lines.append("Comp. SR - {0[overall_stats][comprank]}")
-        lines.append("Time Played - {0[game_stats][time_played]} hours")
-        lines.append("-----")
-        lines.append("Games Won - {0[game_stats][games_won]}")
-        lines.append("Eliminations - {0[game_stats][eliminations]}")
-        lines.append("Deaths - {0[game_stats][deaths]}")
-        lines.append("KDR - {0[game_stats][kpd]}")
-        lines.append("Gold Medals - {0[game_stats][medals_gold]}")
-        lines.append('```')
-        message = '\n'.join(lines)
-        if mode == "competitive":
-            await ctx.channel.send(message.format(data["us"]["stats"]["competitive"]))
-        elif mode == "quickplay":
-            await ctx.channel.send(message.format(data["us"]["stats"]["quickplay"]))
-        else:
-            await ctx.channel.send("Unknown gamemode.")
 
     def get_bot_uptime(self, *, brief=False):
         now = datetime.datetime.utcnow()
