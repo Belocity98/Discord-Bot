3040d525b1f4cb56b046186929efb029dbf36e6f
TheTrain2000 <gibbstyler7@gmail.com>
2017-04-21 19:12:54 -0500

    Remove useless commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -10,9 +10,7 @@ import sys
 import os
 
 from lxml import etree
-from random import randint
 from discord.ext import commands
-from .utils import checks, config
 from urllib.parse import parse_qs
 from datetime import datetime, timezone
 
@@ -22,9 +20,6 @@ class Misc():
 
     def __init__(self, bot):
         self.bot = bot
-        app_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-        cfgfile = os.path.join(app_path, 'misc.json')
-        self.config = config.Config(cfgfile, loop=bot.loop)
 
     @commands.command()
     async def ping(self, ctx):
@@ -35,249 +30,6 @@ class Misc():
         difference = difference/2
         difference = str(round(difference, 2))
         await x.edit(content=f'Pong! `{difference} ms.`')
-
-    async def check_strikes(self, guild, user, ctx):
-        strikes = self.config.get('strikes', {})
-        guild_id = guild.id
-
-        db = strikes.get(guild_id, {})
-        strikeamt = int(self.bot.config["strikes"]["amount"])
-        banlength = int(self.bot.config["strikes"]["ban_length"])
-
-        if db[user.id] >= strikeamt:
-            embed = discord.Embed(description=f'{user.name} reached the max strike limit!')
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-
-            db[user.id] = 0
-            strikes[guild_id] = db
-            await self.config.put('strikes', strikes)
-
-            await self.bot.get_cog("Mod").ban_func(guild, user, message=f"Reaching {strikeamt} strikes.", length=banlength)
-
-            return True
-
-        return False
-
-    @commands.command(no_pm=True)
-    @commands.has_permissions(manage_roles=True)
-    async def maketitle(self, ctx, *, name : str):
-        """This command makes a role with no permissions that can be mentioned by anyone."""
-        guild = ctx.guild
-
-        for role in guild.roles:
-            if role.name.lower() == name.lower():
-                embed = discord.Embed(description='A role with that name already exists!')
-                embed.colour = 0x1BE118 # lucio green
-                await ctx.channel.send(embed=embed)
-                return
-
-        permissions = discord.Permissions()
-        color = discord.Color(value=0).orange()
-
-        await guild.create_role(color=color, name=name, hoist=True, mentionable=True, permissions=permissions)
-
-    @commands.command(no_pm=True)
-    @commands.cooldown(1, 10, commands.BucketType.guild)
-    async def lying(self, ctx, user : discord.Member):
-        """Keep record of a lie that someone says."""
-        guild = ctx.guild
-        author = ctx.author
-
-        lie_channel = discord.utils.find(lambda c: c.name == 'lie-channel', guild.channels)
-        if lie_channel == None:
-            ctx.channel.send('Lie channel not found.\nCreate a channel called `lie-channel` to enable this feature.')
-            return
-
-        def author_check(m):
-            return m.author.id == author.id
-
-        embed = discord.Embed(description=f"{user.name} is lying? What are they lying about?\nReply with their lie.")
-        embed.colour = 0x1BE118 # lucio green
-        await ctx.channel.send(embed=embed)
-        lie = await self.bot.wait_for('message', timeout=60, check=author_check)
-
-        embed = discord.Embed(title=f'{user.name} was caught lying!')
-        embed.description = f'Exposed by: {author.name}'
-        embed.timestamp = ctx.message.created_at
-        embed.add_field(name='Lie', value=lie.content)
-        embed.colour = 0x1BE118 # lucio green
-
-        await lie_channel.send(embed=embed)
-
-        embed = discord.Embed(description="Lie logged!")
-        embed.colour = 0x1BE118 # lucio green
-        await ctx.channel.send(embed=embed)
-
-    @commands.group(no_pm=True, invoke_without_command=True)
-    @commands.cooldown(1, 5.1, commands.BucketType.user)
-    async def strike(self, ctx, user : discord.Member):
-        """Strike somebody. 25 strikes bans for 10 seconds."""
-        guild = ctx.guild
-        author = ctx.author
-
-        strikes = self.config.get('strikes', {})
-        guild_id = ctx.guild.id
-        db = strikes.get(guild_id, {})
-
-        if user.bot == True:
-            embed = discord.Embed(description="Don't try to strike a lowly bot!")
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-
-        log.info(f'{author.name} striked {user.name} in {guild.name}.')
-        if user.id in db:
-            db[user.id] += 1
-            strikes[guild_id] = db
-            await self.config.put('strikes', strikes)
-            strike_check = await self.check_strikes(guild, user, ctx)
-            if strike_check == False:
-                embed = discord.Embed(description=f'{user.name} now has {db[user.id]} strikes.')
-                if db[user.id] == 1:
-                    embed = discord.Embed(description=f'{user.name} now has 1 strike.')
-                embed.colour = 0x1BE118 # lucio green
-                await ctx.channel.send(embed=embed)
-
-        else:
-            db[user.id] = 1
-            strikes[guild_id] = db
-            await self.config.put('strikes', strikes)
-            embed = discord.Embed(description=f'{user.name} now has {db[user.id]} strike.')
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-
-    @strike.command(name='list')
-    async def strike_list(self, ctx, user : discord.Member):
-        guild = ctx.guild
-
-        guild_id = guild.id
-        strikes = self.config.get('strikes', {})
-        db = strikes.get(guild_id, {})
-
-        if user.id not in db:
-            embed = discord.Embed(description=f'{user.name} has 0 strikes.')
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-        if int(db[user.id]) == 1:
-            embed = discord.Embed(description=f'{user.name} has 1 strike.')
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-        embed = discord.Embed(description=f'{user.name} has {db[user.id]} strikes.')
-        embed.colour = 0x1BE118 # lucio green
-        await ctx.channel.send(embed=embed)
-        return
-
-    @strike.command(name='add')
-    @commands.has_permissions(ban_members=True)
-    async def strike_add(self, ctx, user : discord.Member, amount : int):
-        """Add strikes to a user."""
-        guild = ctx.guild
-        author = ctx.author
-
-        strikes = self.config.get('strikes', {})
-        guild_id = ctx.guild.id
-        db = strikes.get(guild_id, {})
-
-        if user.bot == True:
-            embed = discord.Embed(description="Don't try to strike a lowly bot!")
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-
-        if amount < 0:
-            embed = discord.Embed(description="You can't add negative strikes!")
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-
-        if user.id in db:
-            log.info(f'{author.name} added {amount} strikes to {user.name} in {guild.name}.')
-            db[user.id] += amount
-            strikes[guild_id] = db
-            await self.config.put('strikes', strikes)
-            strike_check = await self.check_strikes(guild, user, ctx)
-            if strike_check == False:
-                embed = discord.Embed(description=f'{user.name} now has {db[user.id]} strikes.')
-                if db[user.id] == 1:
-                    embed = discord.Embed(description=f'{user.name} now has 1 strike.')
-                embed.colour = 0x1BE118 # lucio green
-                await ctx.channel.send(embed=embed)
-
-        else:
-            log.info(f'{author.name} added {amount} strikes to {user.name} in {guild.name}.')
-            db[user.id] = amount
-            strikes[guild_id] = db
-            await self.config.put('strikes', strikes)
-            embed = discord.Embed(description=f'{user.name} now has {db[user.id]} strikes.')
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-
-    @strike.command(name='remove')
-    @commands.has_permissions(ban_members=True)
-    async def strike_remove(self, ctx, user : discord.Member, amount : int):
-        """Remove strikes from a user."""
-        guild = ctx.guild
-        author = ctx.author
-
-        strikes = self.config.get('strikes', {})
-        guild_id = ctx.guild.id
-        db = strikes.get(guild_id, {})
-
-        if user.bot == True:
-            embed = discord.Embed(description="Don't try to strike a lowly bot!")
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-
-        if amount < 0:
-            embed = discord.Embed(description="You can't remove negative strikes!")
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-
-
-        if db[user.id] == 0:
-            embed = discord.Embed(description='That user has no strikes.')
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-            return
-
-        if user.id in db:
-            log.info(f'{author.name} removed {amount} strikes from {user.name} in {guild.name}.')
-            db[user.id] -= amount
-            strikes[guild_id] = db
-            await self.config.put('strikes', strikes)
-            if db[user.id] < 0:
-                db[user.id] = 0
-                strikes[guild_id] = db
-                await self.config.put('strikes', strikes)
-            embed = discord.Embed(description=f'{user.name} now has {db[user.id]} strikes.')
-            embed.colour = 0x1BE118 # lucio green
-            await ctx.channel.send(embed=embed)
-
-    @strike.command(name='reset')
-    @commands.has_permissions(manage_guild=True)
-    async def strike_reset(self, ctx):
-        """Resets strikes for a guild."""
-        guild = ctx.guild
-        author = ctx.author
-
-        strikes = self.config.get('strikes', {})
-        guild_id = ctx.guild.id
-
-        db = strikes.get(guild_id, {})
-        db = {}
-        strikes[guild_id] = db
-        await self.config.put('strikes', strikes)
-
-        embed = discord.Embed(description='All strikes in guild reset.')
-        embed.colour = 0x1BE118 # lucio green
-        await ctx.channel.send(embed=embed)
-
-        log.info(f'{author.name} reset all strikes in {esrver.name}.')
 
     @commands.command(no_pm=True)
     async def quote(self, ctx, user : discord.Member, message_id : int=None):
@@ -309,6 +61,70 @@ class Misc():
         embed.colour = 0x1BE118 # lucio green
 
         await ctx.channel.send(embed=embed)
+
+    @commands.command(aliases=['google'])
+    async def g(self, ctx, *, query):
+        """Searches google and gives you top result."""
+        channel = ctx.channel
+
+        try:
+            card, entries = await self.get_google_entries(query)
+        except RuntimeError as e:
+            await channel.send(str(e))
+        else:
+            if card:
+                value = '\n'.join(entries[:3])
+                if value:
+                    card.add_field(name='Search Results', value=value, inline=False)
+                return await channel.send(embed=card)
+
+            if len(entries) == 0:
+                return await channel.send('No results found... sorry.')
+
+            next_two = entries[1:3]
+            if next_two:
+                formatted = '\n'.join(map(lambda x: '<%s>' % x, next_two))
+                msg = '{}\n\n**See also:**\n{}'.format(entries[0], formatted)
+            else:
+                msg = entries[0]
+
+            await channel.send(msg)
+
+    @commands.command()
+    async def xkcd(self, ctx, number=None):
+        """Sends the link of a random xkcd comic to the chat.
+        if a number is specified, send the comic referring to that number."""
+
+        if number:
+            comic = xkcd.getComic(number)
+        else:
+            comic = xkcd.getRandomComic()
+
+        comicurl = comic.getImageLink()
+
+        embed = discord.Embed(description=f'[Image Link]({comicurl})')
+        embed.set_image(url=comicurl)
+        embed.colour = 0x1BE118 # lucio green
+
+        try:
+            await ctx.channel.send(embed=embed)
+        except:
+            pass
+
+    @commands.command()
+    async def urband(self, ctx, *, word : str):
+        """Sends the definition of a word from the UrbanDictionary."""
+
+        word_list = urbandict.define(word)
+
+        embed = discord.Embed(title=word_list[0]['word'])
+        embed.description = word_list[0]['def']
+
+        embed.colour = 0x1BE118 # lucio green
+
+        await ctx.channel.send(embed=embed)
+
+
 
     def parse_google_card(self, node):
         if node is None:
@@ -521,123 +337,6 @@ class Misc():
 
         return card, entries
 
-    @commands.command(aliases=['google'])
-    async def g(self, ctx, *, query):
-        """Searches google and gives you top result."""
-        channel = ctx.channel
-
-        try:
-            card, entries = await self.get_google_entries(query)
-        except RuntimeError as e:
-            await channel.send(str(e))
-        else:
-            if card:
-                value = '\n'.join(entries[:3])
-                if value:
-                    card.add_field(name='Search Results', value=value, inline=False)
-                return await channel.send(embed=card)
-
-            if len(entries) == 0:
-                return await channel.send('No results found... sorry.')
-
-            next_two = entries[1:3]
-            if next_two:
-                formatted = '\n'.join(map(lambda x: '<%s>' % x, next_two))
-                msg = '{}\n\n**See also:**\n{}'.format(entries[0], formatted)
-            else:
-                msg = entries[0]
-
-            await channel.send(msg)
-
-    @commands.command()
-    async def xkcd(self, ctx, number=None):
-        """Sends the link of a random xkcd comic to the chat.
-        if a number is specified, send the comic referring to that number."""
-
-        if number:
-            comic = xkcd.getComic(number)
-        else:
-            comic = xkcd.getRandomComic()
-
-        comicurl = comic.getImageLink()
-
-        embed = discord.Embed(description=f'[Image Link]({comicurl})')
-        embed.set_image(url=comicurl)
-        embed.colour = 0x1BE118 # lucio green
-
-        try:
-            await ctx.channel.send(embed=embed)
-        except:
-            pass
-
-    @commands.command()
-    async def urband(self, ctx, *, word : str):
-        """Sends the definition of a word from the UrbanDictionary."""
-
-        word_list = urbandict.define(word)
-
-        embed = discord.Embed(title=word_list[0]['word'])
-        embed.description = word_list[0]['def']
-
-        embed.colour = 0x1BE118 # lucio green
-
-        await ctx.channel.send(embed=embed)
-
-    @commands.command(no_pm=True)
-    async def discrimsearch(self, ctx, discrim : str=None):
-        """Sends the first 10 discrims found of the given discrim.
-
-        If no discrim is provided, author's discrim is used."""
-
-        embed = discord.Embed()
-        embed.colour = 0x1BE118 # lucio green
-
-        if not discrim:
-            discrim = ctx.author.discriminator
-
-        discrim_users = []
-        for guild in self.bot.guilds:
-            for member in guild.members:
-                if member.discriminator == discrim:
-                    discrim_users.append(f'{member.name}#{member.discriminator}')
-
-        discrim_users = list(set(discrim_users))
-        total_matches = len(discrim_users)
-
-        if total_matches == 0:
-            embed.description = 'No matches found.'
-            await ctx.send(embed=embed)
-            return
-
-        extra = None
-        if total_matches > 10:
-            extra = total_matches - 10
-
-        if extra:
-            embed.description = '\n'.join(discrim_users[:10]) + f'\n\nAnd {extra} more.'
-        else:
-            embed.description = '\n'.join(discrim_users)
-
-        await ctx.send(embed=embed)
-
-    @commands.command(no_pm=True)
-    @commands.has_permissions(manage_roles=True)
-    async def autoassign(self, ctx, role : discord.Role=None):
-        """Autoassign a specific role upon member join."""
-        server = ctx.server
-
-        all_autoassigns = self.config.get('autoassign_roles', {})
-
-        if not role:
-            try:
-                del all_autoassigns[server.id]
-            except:
-                pass
-            await self.config.put('autoassign_roles', all_autoassigns)
-            return
-
-        all_autoassigns[server.id] = role.id
-        await self.config.put('autoassign_roles', all_autoassigns)
 
 
 def setup(bot):
