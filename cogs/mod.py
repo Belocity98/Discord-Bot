deaa4749f6368df15150f72d9f8181b28cdd4ebd
TheTrain2000 <gibbstyler7@gmail.com>
2017-07-10 15:03:51 -0500

    [mod] Add description for softban command

--- cogs/mod.py
+++ cogs/mod.py
@@ -19,6 +19,7 @@ class Mod:
     @commands.command(no_pm=True)
     @commands.has_permissions(ban_members=True)
     async def softban(self, ctx, member : discord.Member, length : int, *, reason : str='No reason given.'):
+        """Temporarily bans a member for a specified amount of time."""
 
         if length > 10000:
             return await ctx.send('Length of ban is too long.')
